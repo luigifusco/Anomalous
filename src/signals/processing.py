9c87db63a8bc4923362a3a6e5cc0f6b82746f079
ian-ofgod <ian.didio@mail.polimi.it>
2020-05-15 17:13:59 +0200

    added functions for bradycardia and tachycardia levels

--- src/signals/processing.py
+++ src/signals/processing.py
@@ -24,18 +24,90 @@ def filter_signal(signal, sample_rate, n_samp):
 
 def obtain_bpm(ecg_signal, sample_rate, start_at, finish_at):
     """
-
-
-    :param ecg_signal:
-    :param sample_rate:
-    :param start_at:
-    :param finish_at:
-    :return:
+    calculate bpm filtering the signal and upsampling it for better results
+    :param ecg_signal: 1 lead from an ecg
+    :param sample_rate: the sample rate of the ecg
+    :param start_at: this defines the start portion of the ecg that you want to analyse
+    :param finish_at: this defines the end portion of the ecg that you want to analyse
+    :return: the bpm for the given sequence
     """
-    filtered = hp.filter_signal(ecg_signal[start_at:finish_at], cutoff=5, sample_rate=sample_rate, filtertype='highpass')
+    filtered = hp.filter_signal(ecg_signal[start_at:finish_at], cutoff=5, sample_rate=sample_rate,
+                                filtertype='highpass')
     upsampled = scipy.signal.resample(filtered, len(filtered) * 4)
     wd, m = hp.process(hp.scale_data(upsampled), sample_rate * 4)
 
     for measure in m.keys():
         if measure == 'bpm':
             return m[measure]
+
+
+def tachycardia_level(age, bpm):
+    """
+    This function returns the level 0->1->2 where 2 is the maximum of tachycardia
+    :param age: the age of the patient
+    :param bpm: the registered bpm
+    :return: the level of tachycardia
+    """
+    if age <= 45:
+        if bpm > 180:
+            return 2
+        elif bpm > 153:
+            return 1
+        else:
+            return 0
+    elif age <= 50:
+        if bpm > 175:
+            return 2
+        elif bpm > 149:
+            return 1
+        else:
+            return 0
+    elif age <= 55:
+        if bpm > 170:
+            return 2
+        elif bpm > 145:
+            return 1
+        else:
+            return 0
+    elif age <= 60:
+        if bpm > 165:
+            return 2
+        elif bpm > 140:
+            return 1
+        else:
+            return 0
+    elif age <= 65:
+        if bpm > 160:
+            return 2
+        elif bpm > 136:
+            return 1
+        else:
+            return 0
+    elif age <= 70:
+        if bpm > 155:
+            return 2
+        elif bpm > 132:
+            return 1
+        else:
+            return 0
+    elif age > 70:
+        if bpm > 150:
+            return 2
+        elif bpm > 128:
+            return 1
+        else:
+            return 0
+
+
+def bradycardia_level(bpm):
+    """
+    Returns the bradycardia level for the corresponding bpm
+    :param bpm: the revealed bpm
+    :return: the level
+    """
+    if bpm > 60:
+        return 0
+    elif bpm > 50:
+        return 1
+    else:
+        return 2
